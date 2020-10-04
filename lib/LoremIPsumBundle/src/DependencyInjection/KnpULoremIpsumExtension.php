e23a51a9028422f611475340c0d550511bc05290
mohammedelattar <mohammed.elattar@metro-markets.de>
2020-10-04 07:52:30 +0200

    add configuration to the bundle

--- lib/LoremIPsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ lib/LoremIPsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -9,9 +9,22 @@ use Symfony\Component\Config\FileLocator;
 
 class KnpULoremIpsumExtension extends Extension
 {
+    public function getAlias(): string
+    {
+        return 'knpu_lorem_ipsum';
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->getDefinition('knpu_lorem_ipsum.knpu_ipsum')
+            ->setArgument(0, $config['unicorns_are_real'])
+            ->setArgument(1, $config['min_sunshine'])
+            ;
     }
 }
--- lib/LoremIPsumBundle/src/KnpULoremIpsumBundle.php
+++ lib/LoremIPsumBundle/src/KnpULoremIpsumBundle.php
@@ -2,9 +2,17 @@
 
 namespace KnpU\LoremIpsumBundle;
 
+use KnpU\LoremIpsumBundle\DependencyInjection\KnpULoremIpsumExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KnpULoremIpsumBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new KnpULoremIpsumExtension();
+        }
 
+        return $this->extension;
+    }
 }
